1f4183d72127e4b0b417dddda1acedd7e77449c7
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-04-11 16:11:39 +0530

    nativeRe

--- component/ApiCalls/ApiCall2.jsx
+++ component/ApiCalls/ApiCall2.jsx
@@ -5,6 +5,34 @@ import { Button, ScrollView, Text, TextInput, View, FlatList, Pressable } from '
 
 export default function ApiCall2(){
 
+    const[apiData, setApiData] = useState([])
+
+
+
+
+    useEffect(
+
+        async function getApiData(){
+
+            let incomingData = await fetch('https://dummyjson.com/users?limit=7&select=firstName,age,lastName')
+
+            let finalIncomingData  = await incomingData.json()
+
+            if(finalIncomingData){
+                setApiData(
+                    finalIncomingData.users.map(userItem=> `${userItem.firstName} ${userItem.lastName} age: ${userItem.age}`)
+                )
+            }
+
+            getApiData()
+
+        }
+
+
+    ,[])
+
+
+
     return(
 
         <View>
@@ -15,6 +43,21 @@ export default function ApiCall2(){
 
             <View style={{borderWidth: 2, borderColor: 'pink'}}>
 
+            </View>
+
+            <View>
+                <FlatList 
+                    data={apiData}
+                    renderItem={(itemData)=> 
+                    <Text style={{padding: 15,
+                    borderWidth: 3,
+                    borderColor: 'purple',
+                    borderRadius: 100,
+                    textAlign: 'center',
+                    marginTop: 10}}>
+                        {itemData.item}
+                    </Text>}
+                />
             </View>
 
         </View>
